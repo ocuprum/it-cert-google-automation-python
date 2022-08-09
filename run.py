057a95474cb6fda4582450b1f0fe10dba51a2fec
Oleksandra Mednikova <olecuprum@gmail.com>
2022-08-10 01:09:20 +0300

    Final version run.py

--- run.py
+++ run.py
@@ -2,19 +2,16 @@
 import os
 import requests
 
-desc_directory = 'supplier-data/descriptions'
-url = 'http://[linux-instance-external-IP]/fruits'
+desc_directory = '/home/student-03-f8fa30dee73b/supplier-data/descriptions'
+url = 'http://34.170.105.169/fruits/'
 
 os.chdir(desc_directory)
-descs = []
 for text in os.listdir(desc_directory):
     with open(text) as txt:
         desc = {}
         desc['name'], desc['weight'], desc['description'] = txt.read().strip('\n').split('\n')
         desc['weight'] = int(desc['weight'].rstrip(' lbs'))
         desc['image_name'] = text.rstrip('txt') + 'jpeg'
-        descs.append(desc)
-
-responce = requests.post(url, json=descs)
-if responce.status_code >= 400: 
-    raise 'Bad status code!'
+        response = requests.post(url, json=desc)
+        if response.status_code >= 400: 
+            raise 'Bad status code!'
