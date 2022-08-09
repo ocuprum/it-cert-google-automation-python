a8e87db368d0edbe6b85ff4106e997dbb217ab4c
Oleksandra Mednikova <olecuprum@gmail.com>
2022-08-10 00:58:55 +0300

    Final version supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -2,10 +2,11 @@
 import os
 import requests
 
-imgs_directory = '/supplier-data/images'
-url = '/upload/'
+imgs_directory = '/home/student-03-f8fa30dee73b/supplier-data/images'
+url = 'http://34.170.105.169/upload/'
 
 os.chdir(imgs_directory)
 for image in os.listdir(imgs_directory):
-    with open(image, 'rb') as image:
-        response = requests.post(url, files={'file': image})
+    if image.endswith('.jpeg'):
+        with open(image, 'rb') as image:
+            response = requests.post(url, files={'file': image})
