4e6346b991e7106a668debc43586e40b257dea86
Oleksandra Mednikova <olecuprum@gmail.com>
2022-08-10 02:52:08 +0300

    Final version health_check.py

--- health_check.py
+++ health_check.py
@@ -3,6 +3,7 @@ import socket
 import psutil, shutil
 from emails import generate_email, send_email
 
+subject = ''
 cpu_usage = psutil.cpu_percent(1)
 du = shutil.disk_usage('/')
 disk_usage = du.free / du.total * 100
@@ -20,8 +21,7 @@ elif not localhost_check:
 
 if subject != '':
     sender = 'automation@example.com'
-    recipient = 'username@example.com'
+    recipient = 'student-03-f8fa30dee73b@example.com'
     body = 'Please check your system and resolve the issue as soon as possible.'
-    ap = '/supplier-data/descriptions/processed.pdf'
-    message = generate_email(sender, recipient, subject, body, ap)
+    message = generate_email(sender, recipient, subject, body)
     send_email(message)
