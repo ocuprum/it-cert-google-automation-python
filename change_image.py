68028601710289de1b7453e23a70a10ef5036f72
Oleksandra Mednikova <olecuprum@gmail.com>
2022-08-10 00:51:13 +0300

    Final version change_image.py

--- change_image.py
+++ change_image.py
@@ -3,16 +3,17 @@ import os
 from PIL import Image
 from typing import Tuple
 
-def change_image(img_name: str, size: Tuple[int, int], img_format: str, save_dir: str) -> None:
+def change_image(img_name: str, size: Tuple[int, int], img_format: str) -> None:
     with Image.open(img_name) as img:
         new_img = img.convert('RGB').resize(size)
-        new_img_name = '{}/{}.{}'.format(save_dir, img_name, img_format.lower())
-        new_img.save(save_dir, format=img_format)
+        new_img_name = '{}{}'.format(img_name.rstrip('tiff'), img_format.lower())
+        new_img.save(new_img_name, format=img_format)
 
 # Working with supplier images
-directory = '/supplier-data/images'
-save_directory = '/supplier-data/images'
+directory = '/home/student-03-f8fa30dee73b/supplier-data/images'
+save_directory = '/home/student-03-f8fa30dee73b/supplier-data/images'
 
-os.chdir(directory)
+os.chdir(save_directory)
 for img in os.listdir(directory):
-    change_image(img, (600, 400), 'JPEG') 
+    if img.endswith('.tiff'): 
+        change_image(img, (600, 400), 'JPEG') 
